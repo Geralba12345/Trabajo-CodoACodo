e59569025c86e51017c26345ce578fa8ddc8dc68
aguscelizcaballero <agu.celiz@hotmail.com>
2023-11-02 17:52:07 +0100

    nuevos cambios al formulario y api rest

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,50 @@ toggleBtn.onclick = function () {
 
 }
 
+function validarFormulario() {
+    // Obtener los valores de los campos
+    const nombre = document.getElementById("nombre").value;
+    const email = document.getElementById("email").value;
+    const mensaje = document.getElementById("mensaje").value;
+    const suscripcion = document.getElementById("suscripcion").checked;
+
+    // Validar que los campos no estén vacíos
+    if (nombre === "" || email === "" || mensaje === "" || !suscripcion) {
+        alert("Todos los campos son obligatorios. Asegúrese de completar todos los puntos.");
+        return false;
+    }
+
+    // Validar el formato del correo electrónico
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!email.match(emailRegex)) {
+        alert("Por favor, ingrese una dirección de correo electrónico válida.");
+        return false;
+    }
+
+    // Si todo es correcto, el formulario se enviará
+    return true;
+}
+
+// API REST
+
+const URL = 'https://api.thecatapi.com/v1/images/search';
+
+const img = document.querySelector('.gatitos');
+const cambiarFotoButton = document.getElementById('cambiarFoto');
+
+function obtenerNuevaFoto() {
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => {
+      img.src = data[0].url;
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+
+// Llama a la función para obtener la primera foto cuando se carga la página
+obtenerNuevaFoto();
+
+// Cambiar la foto cuando se hace clic
+cambiarFotoButton.addEventListener('click', obtenerNuevaFoto);
